ca0dfd2691597f784047caf921265182613d7a4c
Andika Chamberlin <htmlandika@gmail.com>
2019-07-10 23:09:25 +0700

    Add files via upload

---
+++ assets/js/dev.script.js
@@ -0,0 +1,12 @@
+/*------------------------------------------------------------------
+
+[Master Dev Script]
+
+Organization     : Flamecore 
+Organization URI : http://flamecore.pro
+Name Project     : 
+Version          : 1.0.0
+Author           : Andika Chamberlin
+Author URI       : http://andikachamberlin.github.io
+
+-------------------------------------------------------------------*/
